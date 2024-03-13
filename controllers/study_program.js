76d8af9738f80d387dfa4a94642a3c7d49e55348
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-03-14 03:18:58 +0700

    feat: add params get all study programs

--- controllers/study_program.js
+++ controllers/study_program.js
@@ -1,16 +1,30 @@
 const { studyProgramSvc } = require('../services');
 const { studyProgram: studyProgramTransformer } = require('../common/response_transformer');
+const paginate = require('../utils/generate_pagination');
 
 module.exports = {
     index: async (req, res, next) => {
         try {
-            const studyPrograms = await studyProgramSvc.getAllStudyProgram();
+            let {
+                page = "1", limit = "10", search = "", options = "true"
+            } = req.query;
+
+            page = parseInt(page);
+            limit = parseInt(limit);
+            let start = 0 + (page - 1) * limit;
+            let end = page * limit;
+
+            const studyPrograms = await studyProgramSvc.getAllStudyProgram(start, limit, search, options);
+
+            let pagination = null;
+            if (options == 'false') pagination = paginate(studyPrograms.count, studyPrograms.rows.length, limit, page, start, end);
 
             return res.status(200).json({
                 status: 'OK',
                 message: 'Get All Study Program Success',
-                data: studyProgramTransformer.studyProgramList(studyPrograms)
-            })
+                pagination,
+                data: studyProgramTransformer.studyProgramList(studyPrograms.rows)
+            });
         } catch (error) {
             next(error);
         }
--- services/study_program.js
+++ services/study_program.js
@@ -1,13 +1,29 @@
 const { StudyProgram, Faculty } = require('../models');
+const { Op } = require('sequelize');
 
 module.exports = {
-    getAllStudyProgram: async () => {
+    getAllStudyProgram: async (startPage, limit, search, options) => {
         try {
-            const stdprograms = await StudyProgram.findAll({
+            let opts = {};
+            if (options == 'false') {
+                opts = {
+                    limit: limit,
+                    offset: startPage,
+                    distinct: true
+                }
+            }
+            const stdprograms = await StudyProgram.findAndCountAll({
+                where: {
+                    name: { [Op.iLike]: `%${search}%` }
+                },
                 include: [
                     { model: Faculty, as: 'faculty' }
                 ],
-                order: [['faculty_id', 'ASC'], ['name', 'ASC']]
+                order: [
+                    ['faculty_id', 'ASC'],
+                    ['name', 'ASC']
+                ],
+                ...opts
             });
 
             return stdprograms;
