35db5f0dbe4a1e408289b793e0d27d2bb56941ae
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-03-14 03:34:36 +0700

    feat: add create new study program

--- routes/study_program.js
+++ routes/study_program.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { studyProgram } = require('../controllers');
 const authorize = require('../middlewares/authorize');
+const role = require('../common/role');
 
 router.get('/', authorize(), studyProgram.index);
 router.get('/:id', authorize(), studyProgram.show);
+router.post('/', authorize([role.SUPER_ADMIN]), studyProgram.create);
 
 module.exports = router;
